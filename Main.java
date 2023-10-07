50d202551f61ea711fe8e9c4160422ec9a4c4032
Taslim <taslimharuna21@gmail.com>
2023-10-07 22:05:47 +0800

    tess

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+package pkg100daysofcoding;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Aku bisa tersenyum ke semua orang, tapi percayalah.. bahwa cintaku hanya untuk kau seorang");
+    }
+    
+}
